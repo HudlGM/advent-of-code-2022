94164f7d4a6d128bd43dcf8f22a075f0210b125a
George Miller <george.miller@hudl.com>
2022-12-02 23:44:52 -0500

    run.sh: accept help flags

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ] || ([ $# -eq 1 ] && [[ "$1" =~ [Hh-] ]]); then
   echo "Usage: $0 <required: day> <optional: v for verbose logging>"
   exit
 elif [ $# -eq 2 ] && [[ "$2" == "v" ]]; then
