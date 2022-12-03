fdff8491973a85d00d14a29523dcd979bbfe5da2
George Miller <george.miller@hudl.com>
2022-12-02 23:38:50 -0500

    run.sh: clean up string comparison

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 if [ $# -eq 0 ]; then
   echo "Usage: $0 <required: day> <optional: v for verbose logging>"
   exit
-elif [ $# -eq 2 ] && [ $2 == "v" ]; then
+elif [ $# -eq 2 ] && [[ "$2" == "v" ]]; then
   VERBOSE="-D VERBOSE"
 fi
 
