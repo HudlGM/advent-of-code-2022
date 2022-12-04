de5567fbb875dc7eee27e6f46ba3831ce53cfca7
George Miller <george.miller@hudl.com>
2022-12-03 22:24:16 -0500

    day 2 part 2

--- 2.cpp
+++ 2.cpp
@@ -8,6 +8,13 @@ enum class Type
   SCISSORS,
 };
 
+enum class Outcome
+{
+  LOSE,
+  DRAW,
+  WIN,
+};
+
 Type readType(const char &c)
 {
   if ((c == 'A') || (c == 'X')) {
@@ -28,6 +35,59 @@ Type readType(const char &c)
       std::cout << c << "=Scissors, ";
 #endif
   return Type::SCISSORS;
+}
+
+Outcome readDesiredOutcome(const char &c)
+{
+  if (c == 'X') {
+#ifdef VERBOSE
+      std::cout << c << "=Lose, ";
+#endif
+    return Outcome::LOSE;
+  }
+
+  if (c == 'Y') {
+#ifdef VERBOSE
+      std::cout << c << "=Draw, ";
+#endif
+    return Outcome::DRAW;
+  }
+
+#ifdef VERBOSE
+      std::cout << c << "=Win, ";
+#endif
+  return Outcome::WIN;
+}
+
+Type selectTypeForDesiredOutcome(const Type &opponent, const Outcome &desiredOutcome)
+{
+  if (desiredOutcome == Outcome::DRAW) {
+    return opponent;
+  }
+
+  if (desiredOutcome == Outcome::LOSE) {
+    if (opponent == Type::ROCK) {
+      return Type::SCISSORS;
+    }
+
+    if (opponent == Type::PAPER) {
+      return Type::ROCK;
+    }
+
+    return Type::PAPER;
+  }
+
+  // otherwise, win
+
+  if (opponent == Type::ROCK) {
+    return Type::PAPER;
+  }
+
+  if (opponent == Type::PAPER) {
+    return Type::SCISSORS;
+  }
+
+  return Type::ROCK;
 }
 
 int typeToPoints(const Type &t)
@@ -58,11 +118,11 @@ int typeToPoints(const Type &t)
   }
 }
 
-int outcomeToPoints(const Type &opponent, const Type &player)
+int playerTotalForMatch(const Type &opponent, const Type &player)
 {
   if (opponent == player) {
 #ifdef VERBOSE
-      std::cout << "Draw" << std::endl;
+      std::cout << "Draw, ";
 #endif
     return 3;
   }
@@ -73,27 +133,28 @@ int outcomeToPoints(const Type &opponent, const Type &player)
     ((player == Type::PAPER   ) && (opponent == Type::ROCK    ))
   ) {
 #ifdef VERBOSE
-      std::cout << "Win" << std::endl;
+      std::cout << "Win, ";
 #endif
     return 6;
   }
 
 #ifdef VERBOSE
-      std::cout << "Loss" << std::endl;
+      std::cout << "Loss, ";
 #endif
   return 0;
 }
 
 int computeScore(const Type &opponent, const Type &player)
 {
-  return typeToPoints(player) + outcomeToPoints(opponent, player);
+  return typeToPoints(player) + playerTotalForMatch(opponent, player);
 }
 
 int main()
 {
   std::ifstream in("2.in");
 
-  int total = 0;
+  int totalUsingPart1Strategy = 0;
+  int totalUsingPart2Strategy = 0;
 
   std::string s;
 
@@ -101,14 +162,29 @@ int main()
 
   while (in) {
     const Type o = readType(s.at(0));
-    const Type p = readType(s.at(2));
+    const Type p = readType(s.at(2)); // part 1 assumes second column is player type
 
-    total += computeScore(o,p);
+    const Outcome desiredOutcome = readDesiredOutcome(s.at(2)); // part 2 understands second column is desired outcome
+
+#ifdef VERBOSE
+      std::cout << "(strategy 1) ";
+#endif
+    totalUsingPart1Strategy += computeScore(o,p);
+
+#ifdef VERBOSE
+      std::cout << "(Strategy 2) ";
+#endif
+    totalUsingPart2Strategy += computeScore(o,selectTypeForDesiredOutcome(o, desiredOutcome));
+
+#ifdef VERBOSE
+      std::cout << std::endl;
+#endif
 
     std::getline(in, s);
   }
 
-  std::cout << std::endl << "Total: " << total << std::endl;
+  std::cout << std::endl << "totalUsingPart1Strategy: " << totalUsingPart1Strategy << std::endl;
+  std::cout << std::endl << "totalUsingPart2Strategy: " << totalUsingPart2Strategy << std::endl;
 
   return 0;
 }
