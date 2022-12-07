01cdb60335194a2a5c1a84c0e97f249d2fdf32f8
George Miller <george.miller@hudl.com>
2022-12-06 22:53:50 -0500

    build: add more warnings

--- run.sh
+++ run.sh
@@ -7,7 +7,9 @@ elif [ $# -eq 2 ] && [[ "$2" == "v" ]]; then
   VERBOSE="-D VERBOSE"
 fi
 
-if g++ $VERBOSE -Wall -Wextra -Werror -Wpedantic "puzzles/$1/main.cpp"; then
+WARNINGS="-Wall -Wextra -Werror -Wpedantic -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused"
+
+if g++ $VERBOSE $WARNINGS "puzzles/$1/main.cpp"; then
   ./a.out
 fi
 
