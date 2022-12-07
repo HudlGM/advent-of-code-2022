eb3e7584d4e101a2c2f4e8fcc1227c18983a44d4
George Miller <george.miller@hudl.com>
2022-12-07 14:48:29 -0500

    build: remove some warnings which clang did not like

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ elif [ $# -eq 2 ] && [[ "$2" == "v" ]]; then
   VERBOSE="-D VERBOSE"
 fi
 
-WARNINGS="-Wall -Wextra -Werror -Wpedantic -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused"
+WARNINGS="-Wall -Wextra -Werror -Wpedantic -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wmissing-declarations -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused"
 
 if g++ -std=c++17 $VERBOSE $WARNINGS "puzzles/$1/main.cpp"; then
   ./a.out
